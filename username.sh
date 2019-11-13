314f546d67a085176cc6c8fe33dab7d3bf959149
Senko723 <senko@chapman.edu>
2019-11-12 22:17:52 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Peter Senko
 echo "Enter a username that start with a lowercase letter and be between 3 and 12 characters. The digits after can only be lowercase letter, digits, and underscores."
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][a-z,0-9_]{2,11}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a username that starts with a lowercase letter and be  between 3 and 12 characters!!!!!It must can also only contain characters that are lowercase, digits, and underscores!!!"
 	echo "Enter a username:  "
